0ff7a0fea286dcf332caedf4b9c53be8b4b257cb
Bonolo Jeanet <bonolojeanet@gmail.com>
2026-05-27 16:41:18 +0200

    Changes made

--- playwright.config.js
+++ playwright.config.js
@@ -1,13 +1,17 @@
+// playwright.config.js
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-
+  reporter: [
+    ['json', { outputFile: 'test-results.json' }],
+    ['junit', { outputFile: 'junit.xml' }],
+    ['html', { outputFolder: 'playwright-report' }]
+  ],
   use: {
-    browserName: 'chromium',
-    headless: false,
+    baseURL: 'https://www.saucedemo.com',
+    headless: true,
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-    trace: 'on-first-retry'
+    video: 'retain-on-failure'
   }
 });
