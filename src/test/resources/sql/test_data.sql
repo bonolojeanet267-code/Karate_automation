1e4377b604312058d4a6c7f1553984d59ee8218b
Bonolo Jeanet <bonolojeanet@gmail.com>
2026-06-05 11:09:03 +0200

    Added sql files

--- src/test/resources/sql/test_data.sql
+++ src/test/resources/sql/test_data.sql
@@ -1,7 +1,7 @@
 INSERT INTO orders
-VALUES(2, 'Standard User', 'Sauce Labs Onesie', 7.99, 7.99, CURRENT_TIMESTAMP, '0712345678'),
-      (3, 'Mpho User', 'Bolt T-Shirt', 15.99, 15.99,CURRENT_TIMESTAMP, '0762384122');
+VALUES(4, 'Akhona Test', 'Sauce Labs Bike light', 9.99, 9.99, CURRENT_TIMESTAMP, '0864267788');
 
-UPDATE orders
-SET phone_number = '0674860705'
-WHERE order_id = 1;
+
+# UPDATE orders
+# SET phone_number = '0674860705'
+# WHERE order_id = 1;
